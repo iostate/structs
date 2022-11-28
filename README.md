38e3ab2d4b89eb035a6bb252fa194d8f86758228
iostate <qmtruong92@gmail.com>
2022-11-28 12:47:03 -0700

    enums, ooptions, match, structs, bin and lib, module sys.

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # structs
+https://www.youtube.com/watch?v=n3bPhdiJm9I&list=PLai5B987bZ9CoVR-QEIN9foz4QCJ0H2Y8&index=6
+
+# OPTIONS_ENUMS
+
+- Coin example! 
+
+# module system
+
+Need to learn more about how to declare files and modules.
+Currently making tons of new projects, rather just create a file, then add it as a module
+in the program's main.rs. 
+
+Create a binary applications dir. in your program. 
+src/bin/guessing_game.rs
+src/bin/calculate_altitude.rs
--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,22 @@
-#[allow(dead_code)]
-#[warn(dead_code)]
 struct User {
-    email: String,
     username: String,
+    email: String,
     sign_in_count: u64,
     active: bool,
 }
 
 fn main() {
-    let mut user1 = User {
-        email: String::from("qmtruong92@gmail.com"),
-        username: String::from("astarfish"),
-        active: true,
-        sign_in_count: 1
-    };
+    struct Color(i32, i32, i32);
+    struct Point(i32, i32, i32);
 
-    let name = user1.username;
-    user1.username = String::from("WallllaceTheLongHairedWeasel");
-
-    let user2 = build_user(
-        String::from("qmtruong92@gmail.com"), 
-        String::from("analyticalstarfish"),
-    );
-
-
-    let user3 = User {
-        email: String::from("ra123@gmail.com"),
-        username: String::from("Tally Hassee"),
-        ..user2
-    };
+    let mut a_point = Point(1, 2, 3);
 }
 
 fn build_user(email: String, username: String) -> User {
     User {
         email,
         username,
-        active: true,
         sign_in_count: 1,
+        active: true,
     }
 }
-
-//fn transform(anything: User) {
-//
-//}
